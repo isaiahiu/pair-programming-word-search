55ca6def0f2265ea97e44d8e63180f3a452b7f93
Isaiah <isaiah.iu@hotmail.com>
2022-03-17 02:18:37 +0000

    assertions all passed

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  const transpose = function (letters) {
+    let newArray = [];
+    for (let i = 0; i < letters[0].length; i++) {
+      let smolArr = [];
+      for (let j = 0; j < letters.length; j++) {
+        smolArr.push(letters[j][i]);
+      }
+      newArray.push(smolArr);
     }
-}
+    return newArray;
+  };
 
-module.exports = wordSearch
+  const verticalJoin = transpose(letters).map((x) => x.join(""));
+
+  for (let ele of horizontalJoin) {
+    if (ele.includes(word)) {
+      return true;
+    }
+  }
+  for (let ele of verticalJoin) {
+    if (ele.includes(word)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
